1feb4c190dac2ba4221fad7076f0ec6a17ac0ff5
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-08 18:38:12 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,15 +2,11 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
-  def song_count
-    # return the number of songs in a genre
-  end
-
+  # This method returns the size of our artists array that is associated with the Genre class.
+  # If an artist gets saved from the Song class as an attribute and that song is associated with our Genre class
+  # This adds the artist into our artist array, thus making the size of our 'artists' array bigger.
   def artist_count
-    # return the number of artists associated with the genre
+    self.artists.size
   end
 
-  def all_artist_names
-    # return an array of strings containing every musician's name
-  end
 end
